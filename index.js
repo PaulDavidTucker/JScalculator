a94166f1726b633e86aceb62e6bf55aeb186bfe0
PaulDavidTucker <tuckerpauldavid@gmail.com>
2023-01-31 17:59:51 +0000

    Added table as a grid, some basic styling

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ button.addEventListener("click", submit)
 function submit(){
     alert("Button clicked")
 }
+
+function add(){
+    //TODO Implement function
+}
+
+function subtract(){
+    //TODO Implement function
+}
+
+function divide(){
+    //TODO Implement function
+}
+
+function clear(){
+    //TODO Implement function
+}
+
+function ans(){
+    //TODO Implement function
+}
