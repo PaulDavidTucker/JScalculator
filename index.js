648a0d5b29958446665e713a23af63f9b0893a92
PaulDavidTucker <tuckerpauldavid@gmail.com>
2023-01-31 19:27:14 +0000

    Help

--- index.js
+++ index.js
@@ -2,26 +2,48 @@ var button = document.getElementById('Submit')
 
 button.addEventListener("click", submit)
 
-function submit(){
-    alert("Button clicked")
+var numberButtons = document.querySelectorAll("NumberButton")
+var FunctionButtons = document.querySelectorAll("FunctionButton")
+var ActionButton = document.querySelectorAll("ActionButton")
+var display = document.getElementById("NumberDisplay")
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement
+      this.currentOperandTextElement = currentOperandTextElement
+      this.clear()
+    }
+
+    calculate(operation, ){
+        alert("Button clicked")
+    }
+    
+     clear(){
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+
+    appendNumber(number){
+        this.currentOperand = number
+    }
+
+    updateDisplay(){
+        display.innerText = this.currentOperand
+        console.log(thi.currentOperand)
+    }
+    
+    
 }
 
-function add(){
-    //TODO Implement function
-}
+var Calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
-function subtract(){
-    //TODO Implement function
-}
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      Calculator.appendNumber(button.innerText)
+      Calculator.updateDisplay()
+      console.log(button.innerText)
+    })
+  })
 
-function divide(){
-    //TODO Implement function
-}
 
-function clear(){
-    //TODO Implement function
-}
-
-function ans(){
-    //TODO Implement function
-}
