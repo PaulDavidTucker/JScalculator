8c9a4193b11f9ef68d33f0fa7af714e93ad4d55d
PaulDavidTucker <tuckerpauldavid@gmail.com>
2023-05-04 22:12:49 +0100

    Formatting changes

--- js/elements.js
+++ js/elements.js
@@ -1,3 +1,9 @@
+// Author: Paul Tucker
+
+/**
+ * This file contains all the elements that are used in the calculator. These DOM elements are used in the index.js file.
+ */
+
 export var numberButtons = document.querySelectorAll(".NumberButton")
 export var OperationButton = document.querySelectorAll(".OperationButton")
 export var ActionButton = document.querySelectorAll(".ActionButton")
